1bcbd782fd39fdc91c5a8b1257cebfc705853117
Shah Nirmal Dipakkumar <nirmalshah20519@gmail.com>
2025-01-29 18:50:36 +0530

    Angular project setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideClientHydration(), provideAnimationsAsync()]
 };
